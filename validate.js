f170fa113245662c789583e3eac06438457440f6
PamelaBarboza <pamelarabarboza@gmail.com>
2020-11-10 23:53:18 -0300

    res

--- validate.js
+++ validate.js
@@ -1,11 +1,29 @@
 const fetch = require("node-fetch");
 
 module.exports = (validateLinks) => {
-  validateLinks.forEach((element) => {
+  const validateFetch = validateLinks.map((element) => {
+    return fetch(element.links).then((res) => {
+      return {
+          href: res.url,
+          texto: element.texto,
+          file: element.file,
+          status: res.status,
+          statusText: res.statusText
+      };
+    });
+  });
+  
+  Promise.all(validateFetch).then((respArray) => {
+    respArray.forEach((res) => {
+      console.log(res);
+    });
+  });
+
+  /* validateLinks.forEach((element) => {
     fetch(element.links).then((res) => {
       console.log(res.url);
       console.log(res.statusText);
       console.log(res.status);
     });
-  });
+  }); */
 };
